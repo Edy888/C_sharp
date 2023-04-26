159746098de5b5aca988daeb74cd0ec4ab8a9c3e
Eduard Soldatkin <solde@mail.ru>
2023-04-26 17:18:51 +0300

    Example010 is complite

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = {1,22,23,4,18,65,36,27,18};
+int n = array.Length;
+Console.WriteLine(n);
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find) //индекс числа из массива совпадает с числом 4
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
+
+
